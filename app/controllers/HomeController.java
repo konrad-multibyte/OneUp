280378f2aea22e0112d1ed39034133b1248be1a6
Grizzly232 <michael.brady.ie@gmail.com>
2018-03-27 19:03:31 +0100

    Created store and game pages

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.users.User;
 import play.mvc.*;
+import java.util.ArrayList;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -19,4 +20,15 @@ public class HomeController extends Controller {
         return ok(views.html.index.render(User.getWithEmail(session().get("email"))));
     }
 
+    public Result game() {
+        return ok(views.html.game.render(User.getWithEmail(session().get("email")),
+                new models.Game("5", "The Elder Scrolls VI: Skyrim 2", "literally the " +
+                        "same as skyrim but we remastered it and made you pay for mods", null,
+                        59.99, 89, null)));
+    }
+
+    public Result store() {
+        ArrayList<models.Game> gameList = new ArrayList<>();
+        return ok(views.html.store.render(User.getWithEmail(session().get("email")), gameList));
+    }
 }
